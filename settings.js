07a85916b8e47b050a687c9c8a454493f5fc871a
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-01-28 05:55:32 +0100

    hello world

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ module.exports = {
     botCreator: 'MX GΔMΞCØDΞR', // Your name as the bot creator
     botVersion: '1.2.0', // Version of the bot
     adminChatId: '6408716304', // Your Telegram ID for notifications
-    defaultPrefix: ',', //pick anyone from the list under
+    defaultPrefix: '.', //pick anyone from the list under
     availablePrefixes: [
       '.', '!', '#', '$', '%', '&', '*', '+', '-', '=', '^', '@', '~', '?', '|', '<', '>', '`', ':', ',' // don't not change please
     ],
