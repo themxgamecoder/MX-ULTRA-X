a755c3f499845d1a8343856450db3b4a8609be73
MX-GΔMΞCØDΞR <150932410+themxgamecoder@users.noreply.github.com>
2025-01-28 05:54:55 +0100

    hello world

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 module.exports = {
     botName: 'MX-ULTRA-X', // Bot name
     botCreator: 'MX GΔMΞCØDΞR', // Your name as the bot creator
-    botVersion: '1.1.0', // Version of the bot
+    botVersion: '1.2.0', // Version of the bot
     adminChatId: '6408716304', // Your Telegram ID for notifications
     defaultPrefix: ',', //pick anyone from the list under
     availablePrefixes: [
